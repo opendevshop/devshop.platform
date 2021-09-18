5275d5066046586f80bfa57a5a1a16420f500834
Jon Pugh <jon@thinkdrop.net>
2021-09-18 15:11:45 -0400

    Add an action plugin to replace ansible.builtin.shell so we can pipe to files for devshop magic.

---
+++ plugins/actions/shell.py
@@ -0,0 +1,66 @@
+# Copyright: (c) 2021, DevShop Project
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
+#
+# This action plugin extends ansible.builtin.shell to add options for controlling output.
+#
+
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+import copy
+import os
+
+from ansible.plugins.action import ActionBase
+
+
+class ActionModule(ActionBase):
+
+    def run(self, tmp=None, task_vars=None):
+        del tmp  # tmp no longer has any effect
+
+        # Shell module is implemented via command with a special arg
+        self._task.args['_uses_shell'] = True
+
+        # Test to confirm that this plugin is loading.
+        # Any invalid task args passed to the ansible.legacy.command action will fail.
+        # self._task.args['will_break'] = True
+
+        # @TODO:
+        # 1. Add properties for file and redirect_X
+
+        arg_output_file = self._task.args.get('output_file')
+        arg_redirect_stderr = self._task.args.get('redirect_stderr')
+
+        cmd = copy.copy(self._task.args['_raw_params'])
+        if arg_output_file:
+            cmd = "%s > %s 2>&1" % (cmd, arg_output_file)
+            del self._task.args['output_file']
+
+        if arg_redirect_stderr:
+            cmd = "%s 2>&1" % (cmd)
+            del self._task.args['redirect_stderr']
+
+        self._task.args['_raw_params'] = cmd
+        command_action = self._shared_loader_obj.action_loader.get('ansible.legacy.command',
+                                                                   task=self._task,
+                                                                   connection=self._connection,
+                                                                   play_context=self._play_context,
+                                                                   loader=self._loader,
+                                                                   templar=self._templar,
+                                                                   shared_loader_obj=self._shared_loader_obj)
+        result = command_action.run(task_vars=task_vars)
+        result['output_file'] = arg_output_file
+        result['redirect_stderr'] = arg_redirect_stderr
+
+        if arg_output_file:
+            try:
+                file_object = open(arg_output_file)
+                # @TODO: Stick with output? or overwrite stdout?
+                result['output'] = file_object.read()
+
+            except OSError as e:
+                if e.errno == errno.ENOENT:
+                    output = {'exists': False}
+                    module.exit_json(changed=False, stat=output)
+
+        return result
